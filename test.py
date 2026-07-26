bdb9b0cc67ec956e83ebfc21bdfa1836846f7446
Samuel Raj G <samuelgariki11@gmail.com>
2026-07-26 15:38:14 +0530

    delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from utils.audio_processor import process_input
-from core.transcriber import transcribe_all
-
-source ="https://youtu.be/z_7J_iKuSzU"
-chunks = process_input(source)
-print(transcribe_all(chunks))
